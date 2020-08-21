e21305ace495c4554b5f4ceb77d9b3e32b7c9544
Tiago Carreira <5046551+tcarreira@users.noreply.github.com>
2020-08-21 12:46:32 +0100

    Update yaml_loader.py

--- yaml_loader.py
+++ yaml_loader.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 
 import yaml
+import lazyxml
 
 with open("test.yaml", 'r') as stream:
+    yaml_source = yaml.load(stream)
     try:
         print(yaml.load(stream))
     except yaml.YAMLError as exc:
         print(exc)
+        exit
+
+print(lazyxml.dumps(yaml_source, indent=' ' * 4))
